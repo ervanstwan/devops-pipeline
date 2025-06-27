41cf81c97a0b51c37b015d0a29a990d25c9ba57b
Ervan Setiawan <ervanstwan@gmail.com>
2025-06-27 20:18:13 +0700

    update config2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,27 @@
 provider "aws" {
   region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+# Ambil AMI terbaru Amazon Linux 2 secara otomatis
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   tags = {
     Name = "AppServer"
